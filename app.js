ef16a1fa4229bd3b929a0a45eeedb66d769f76fe
nancycrojas <nanclarisaok@gmail.com>
2022-06-23 08:39:02 -0300

    Variables, datos y operadores - ejercicio 2

---
+++ app.js
@@ -0,0 +1,16 @@
+//Heladería
+
+//Crear un programa que pida al usuario ingresar, por separado, tres gustos de helado, y luego muestre un mensaje que diga, por ejemplo, "Aquí tiene su helado de chocolate, dulce de leche y menta granizada".
+
+
+const gusto1 = prompt ("Ingrese su primer gusto");
+const gusto2 = prompt ("Ingrese su segundo gusto");
+const gusto3 = prompt ("Ingrese su tercer gusto");
+alert("Aquí tiene su helado de " + gusto1 + ", " + gusto2 + " " + "y" + " " + gusto3);
+
+
+
+
+
+
+
